deca1962928c560e3c6fde982d35bec7311f2a44
HamSik <109560875+hamsik22@users.noreply.github.com>
2024-02-23 14:26:20 +0900

    Change Residence

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: '황석현',
-  title: 'Poet, playwright and actor',
+  title: 'iOS Developer',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
+    'Residence': 'Korea, Jinju',
     'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
                   <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
                 </a>`,
