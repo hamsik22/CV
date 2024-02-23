ff79ff076c4cb48f7bf92b32e18bc3a715530154
HamSik <109560875+hamsik22@users.noreply.github.com>
2024-02-23 17:46:59 +0900

    Update pdf.js

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -1,11 +1,15 @@
 const Puppeteer = require('puppeteer');
+// 웹 폰트 사용을 위한 CSS 추가
+const webFontLink = '<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet">';
+const modifiedHtml = html.replace('</head>', `${webFontLink}</head>`);
 
 module.exports = async function buildPdf(inputFile, outputFile) {
   const browser = await Puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`file://${inputFile}`, {
-    waitUntil: 'networkidle0'
-  });
+  await page.setContent(modifiedHtml, { waitUntil: 'networkidle0' });
+  // await page.goto(`file://${inputFile}`, {
+  //   waitUntil: 'networkidle0'
+  // });
   await page.pdf({
     path: outputFile,
     format: 'A4',
